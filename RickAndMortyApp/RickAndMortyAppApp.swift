71c9c3d1da55f7d50c74c09b11f9f93a503de801
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2026-01-03 14:41:23 -0300

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Rafael on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

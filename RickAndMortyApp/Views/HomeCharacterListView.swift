da0ca75e20baf5f282ddc9a579bc4ac9a2e0a876
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2026-01-03 14:48:51 -0300

    chore: initial configs

--- RickAndMortyApp/Views/HomeCharacterListView.swift
+++ RickAndMortyApp/Views/HomeCharacterListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeCharacterListView.swift
 //  RickAndMortyApp
 //
 //  Created by Rafael on 03/01/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeCharacterListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeCharacterListView()
 }
